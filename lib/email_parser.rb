e6c38545fbc0200b6caf827fb715a44bc26ff52f
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-20 04:36:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,12 +6,14 @@ class EmailParser
     @csv_emails = csv_emails
   end
   
-  def parse
-    csv_emails.split(/\s*,\s*/)
-  end
-  .flatten.uniq 
-  end
-end
+def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
+
 
 email_addresses = "john@doe.com, person@somewhere.org"
 parse = EmailAdresseParse.new(email_addresses)
